8a0965b9245ac5c678279c61738816d4f271ead6
Johelyn Cordova <jmcordova.contractor@bbva.com>
2026-02-14 18:23:16 -0500

    feat: Adds config view without dynamic data

--- app/src/main/java/com/example/finanzapro/ui/ConfigFragment.kt
+++ app/src/main/java/com/example/finanzapro/ui/ConfigFragment.kt
@@ -1,6 +1,21 @@
 package com.example.finanzapro.ui
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.finanzapro.R
 
-class ConfigFragment : Fragment(R.layout.fragment_configuration)
+class ConfigFragment : Fragment(R.layout.fragment_configuration) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // header
+        view.findViewById<TextView>(R.id.tvHeaderTitle).text = "Configuración"
+
+        // Configuracion de btns
+        view.findViewById<View>(R.id.btnSearch).visibility = View.GONE
+        view.findViewById<View>(R.id.btnFilter).visibility = View.GONE
+    }
+}
