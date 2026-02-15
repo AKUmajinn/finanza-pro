a345d89d4ea8a6c23386c485086c90ab1eed5fa0
KilFex <elivan.1251996@gmail.com>
2026-02-15 11:28:39 -0500

    Refactor: new dashboar

--- app/src/main/java/com/example/finanzapro/ui/DashboardFragment.kt
+++ app/src/main/java/com/example/finanzapro/ui/DashboardFragment.kt
@@ -1,25 +1,42 @@
 package com.example.finanzapro.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.finanzapro.MainActivity //
 import com.example.finanzapro.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.finanzapro.databinding.FragmentDashboardBinding
+
 
 class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+
+
+    private lateinit var btnRegisterSpent: FloatingActionButton
+    private lateinit var binding: FragmentDashboardBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // header
-        view.findViewById<TextView>(R.id.tvHeaderTitle).text = "Inicio"
-        view.findViewById<View>(R.id.btnSearch).visibility = View.VISIBLE
 
+        // header, se cambio por el binding
+        binding.customHeader.tvHeaderTitle.text = "Inicio"
+        binding.customHeader.btnSearch.visibility = View.VISIBLE
 
-        val btnRegister = view.findViewById<Button>(R.id.btnRegisterTransaction)
-        btnRegister.setOnClickListener {
+        btnRegisterSpent= view.findViewById(R.id.register_spent)
+        btnRegisterSpent.setOnClickListener {
             (requireActivity() as MainActivity).replaceFragment(RegisterFragment())
         }
     }
